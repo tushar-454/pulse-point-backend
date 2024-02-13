ebde937c721234b1d2661b811464408a5219c2e0
tushar-454 <imtushar454@gmail.com>
2024-02-13 23:57:28 +0600

    Add dependencies and update server configuration with cors cookie parser and connect with mongodb database with mongoose.

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS'],
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Pulse-point backend is running!');
 });
 
-app.listen(4000, () => {
-  console.log(`Pulse-point backend is running on http://localhost:4000`);
-});
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+    app.listen(port, () => {
+      console.log(`Server is running on  http://localhost:${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
