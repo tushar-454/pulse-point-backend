94c1d2357bbebd5d9c93000d56a32cbdf685e4ec
tushar-454 <imtushar454@gmail.com>
2024-02-14 17:59:55 +0600

    Refactor globalError function in Error/globalError.js

--- Error/globalError.js
+++ Error/globalError.js
@@ -1,5 +1,5 @@
 const globalError = (err, req, res, next) => {
-  const error = new Error(err.message ? err.message : 'Internal server error');
+  const error = err.message ? err.message : 'Internal server error';
   const status = err.status ? err.status : 500;
   res.status(status).json({
     message: error,
